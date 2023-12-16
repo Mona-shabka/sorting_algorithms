f57720439245aef3b027ffbaa04c316d7192e304
Mona-shabka <mona.shabka@yahoo.com>
2023-12-16 14:43:38 -0800

    1

--- sort.h
+++ sort.h
@@ -31,6 +31,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+#define UP 0
+#define DOWN 1
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
